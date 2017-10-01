fff3f810cfd6dfe83ceeda124744b5f17339689a
Wenting Tan <wentingt@umich.edu>
2017-09-30 21:22:14 -0400

    helloworld

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-# addressbook.proto => AddressBookProtos.java
-SRC_DIR=./src/main/proto 
-DST_DIR=./src/main/java 
-protoc -I=$SRC_DIR --java_out=$DST_DIR $SRC_DIR/addressbook.proto
-# test AddPerson.java and ListPeople.java
-mvn exec:java -Dexec.mainClass="com.example.tutorial.AddPerson" -Dexec.args="address_book_file"
-mvn exec:java -Dexec.mainClass="com.example.tutorial.ListPeople" -Dexec.args="address_book_file"
+# target/generated-sources/protobuf/grpc-java
+# target/generated-sources/protobuf/java
+
+# test HelloWorld
+mvn exec:java -Dexec.mainClass="io.grpc.examples.helloworld.HelloWorldServer"
+mvn exec:java -Dexec.mainClass="io.grpc.examples.helloworld.HelloWorldClient"
